00a3fd7077531325f77e00acb8e2951fa28c34df
Rara <118464934+darksister76@users.noreply.github.com>
2023-03-08 14:03:50 +0700

    Delete mca-core directory

--- mca-core/src/main/java/com/gusrylmubarok/multiplicationca/core/challenge/ChallengeGeneratorService.java
+++
@@ -1,8 +0,0 @@
-package com.gusrylmubarok.multiplicationca.core.challenge;
-
-public interface ChallengeGeneratorService {
-    /**
-     * @return a randomly-generated challenge with factors between 11 and 99
-     */
-    Challenge randomChallenge();
-}
